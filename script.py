2bc3276c0840c2568203181d7b7893c9158d73a1
Nathan Palmer <153049405+palmer061397@users.noreply.github.com>
2024-03-17 12:35:11 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+def pattern_search(text, pattern):
+  print("Input Text:", text, "Input Pattern:", pattern)
+  for index in range(len(text)):
+    print("Text Index:", index)
+    match_count = 0
+    for char in range(len(pattern)):
+      print("Pattern Index:", char)
+      if pattern[char] == text[index + char]:
+        match_count += 1
+      else:
+        break
+    if match_count == len(pattern):
+      print(pattern, "found at index", index)
